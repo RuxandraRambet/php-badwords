9897c6c66c3b47028db70e4eed8888aeb926c9a3
RuxandraRambet <ruxandraramb@gmail.com>
2024-01-08 18:14:09 +0100

    feat: added bootstrap

--- index.php
+++ index.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <form action="result.php" method="get">
-        <label for="word">Inserisci una parola</label>
-        <input type="text" name="word" id="word">
+    <div class="container pt-5">
+        <form action="result.php" method="get">
+            <label for="word" class="form-label">Inserisci una parola</label>
+            <input class="form-control"  type="text" name="word" id="word">
 
-        <label for="text">Inserisci un paragrafo</label>
-        <textarea name="text" id="text" cols="30" rows="10"></textarea>
-        
-        <button>Invia</button>
-    </form>
+            <label for="text"  class="form-label mt-4">Inserisci un paragrafo</label>
+            <textarea  class="form-control" name="text" id="text" cols="30" rows="10"></textarea>
+
+            <button class="btn btn-primary mt-4">Invia</button>
+        </form>
+    </div>
+
 </body>
 </html>
